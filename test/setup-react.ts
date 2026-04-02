213cc9dea9de71302b39ecded7b6b406b55cca8e
Gabriele D'Arrigo <darrigo.g@gmail.com>
2026-04-02 21:51:40 +0200

    Mock PointerEvent

--- test/setup-react.ts
+++ test/setup-react.ts
@@ -3,3 +3,13 @@ import "@testing-library/jest-dom";
 import { TextEncoder, TextDecoder } from "util";
 
 Object.assign(global, { TextDecoder, TextEncoder });
+
+// jsdom doesn't implement PointerEvent; polyfill with MouseEvent so
+// @base-ui/react components that dispatch `new PointerEvent(...)` work in tests.
+if (typeof window.PointerEvent === "undefined") {
+  Object.defineProperty(window, "PointerEvent", {
+    value: MouseEvent,
+    writable: true,
+    configurable: true,
+  });
+}
