40fbc79c75800137541956ff38265afe92f53b01
Gabriele D'Arrigo <darrigo.g@gmail.com>
2026-04-19 12:55:11 +0200

    Review fixes, round 2

--- src/app/collection/schemas/searchParams.schema.ts
+++ src/app/collection/schemas/searchParams.schema.ts
@@ -4,9 +4,15 @@ import { Condition, Language, Rarity } from "@database/models";
 import { z } from "zod";
 
 export const collectionSearchParams = z.object({
-  page: z.coerce.number().min(1).default(DEFAULT_PAGE).catch(DEFAULT_PAGE),
+  page: z.coerce
+    .number()
+    .int()
+    .min(1)
+    .default(DEFAULT_PAGE)
+    .catch(DEFAULT_PAGE),
   size: z.coerce
     .number()
+    .int()
     .min(1)
     .default(DEFAULT_PAGE_SIZE)
     .catch(DEFAULT_PAGE_SIZE),
@@ -15,9 +21,15 @@ export const collectionSearchParams = z.object({
   setCode: z.string().optional(),
   colors: z
     .string()
-    .transform((s) => s.split(","))
+    .transform((s) =>
+      s
+        .split(",")
+        .filter((color) => color.length > 0)
+        .map((color) => color.trim()),
+    )
     .pipe(z.array(z.enum(Color)))
-    .optional(),
+    .optional()
+    .catch(undefined),
   rarity: z.enum(Rarity).optional().catch(undefined),
   language: z.enum(Language).optional().catch(undefined),
   condition: z.enum(Condition).optional().catch(undefined),
